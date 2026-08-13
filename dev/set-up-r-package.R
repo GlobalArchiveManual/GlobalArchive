ed478fc151e631a4ecee927b415bebf2cf8db05f
AbbeyGibbons <abbey.gibbons@uwa.edu.au>
2026-08-13 09:44:12 +0800

    making markdown

--- dev/set-up-r-package.R
+++ dev/set-up-r-package.R
@@ -1,33 +1,34 @@
-install.packages(c(
-  "usethis",
-  "devtools",
-  "roxygen2",
-  "testthat",
-  "pkgdown"
-))
+# install.packages(c(
+#   "usethis",
+#   "devtools",
+#   "roxygen2",
+#   "testthat",
+#   "pkgdown"
+# ))
 
-dir.create("R", showWarnings = FALSE)
-dir.create("man", showWarnings = FALSE)
-dir.create("tests", showWarnings = FALSE)
-dir.create("vignettes", showWarnings = FALSE)
+# dir.create("R", showWarnings = FALSE)
+# dir.create("man", showWarnings = FALSE)
+# dir.create("tests", showWarnings = FALSE)
+# dir.create("vignettes", showWarnings = FALSE)
 
-usethis::use_description()
-
-usethis::use_mit_license("Brooke Gibbons")
-usethis::use_readme_rmd()
-usethis::use_testthat()
-usethis::use_package_doc()
-usethis::use_news_md()
-
-usethis::use_pkgdown()
+# usethis::use_description()
+# 
+# usethis::use_mit_license("Brooke Gibbons")
+# usethis::use_readme_rmd()
+# usethis::use_testthat()
+# usethis::use_package_doc()
+# usethis::use_news_md()
+# 
+# usethis::use_pkgdown()
 
 # Copy in _pkgdown.yml and vignettes/user-guide/
 
-# Build the site
+#check the package
 devtools::document()
 devtools::load_all()
 devtools::check()
 
+# Build the site
 pkgdown::clean_site()
 pkgdown::build_site()
 
